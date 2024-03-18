954a217d5030c083c86d9e2d48f78000b1ebdcbb
GAidaraliev <linchesel@gmail.com>
2024-03-18 15:42:20 +0500

    add utils to consumer

--- kafka_commons.py
+++ kafka_commons.py
@@ -1,7 +1,12 @@
 import os
-os.environ['PYSPARK_SUBMIT_ARGS'] = '--packages org.apache.spark:spark-streaming-kafka-0-10_2.12:3.1.1,org.apache.spark:spark-sql-kafka-0-10_2.12:3.1.1 pyspark-shell'
+
+os.environ[
+    'PYSPARK_SUBMIT_ARGS'] = ('--packages org.apache.hadoop:hadoop-aws:2.7.3,com.amazonaws:aws-java-sdk:1.7.4,'
+                              'org.apache.spark:spark-streaming-kafka-0-10_2.12:3.1.1,'
+                              'org.apache.spark:spark-sql-kafka-0-10_2.12:3.1.1 pyspark-shell')
 
 import findspark
+
 findspark.init()
 from pyspark.sql import SparkSession
 
@@ -11,6 +16,10 @@ def create_spark_session(app_name: str):
         SparkSession.builder.appName(app_name)
         .master("local[3]")
         .config("spark.streaming.stopGracefullyOnShutdown", "true")
+        .config("com.amazonaws.services.s3.enableV4", "true")
+        .config("fs.s3a.impl", "org.apache.hadoop.fs.s3a.S3AFileSystem")
+        .config("fs.s3a.access.key", "your_access_AWS_key_ID")
+        .config("fs.s3a.secret.key", "your_access_AWS_secret_key")
         .getOrCreate()
     )
 
@@ -18,7 +27,7 @@ def create_spark_session(app_name: str):
 def read(spark_session: SparkSession, topic_name):
     df = (
         spark_session.readStream.format("kafka")
-        .option("kafka.bootstrap.servers", "b-1.dstimsk.bfn7de.c2.kafka.eu-west-1.amazonaws.com:9092")
+        .option("kafka.bootstrap.servers", "your_bootstrap_server")  # - "more bootstrap-servers"
         .option("subscribe", topic_name)
         .option("startingOffsets", "earliest")
         .load()
